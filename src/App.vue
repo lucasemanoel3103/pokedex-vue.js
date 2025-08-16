021a3d1e52f517c8e490fa078c961afb6006ea30
Lucas emanoel <lucasemanoel625@gmail.com>
2025-08-16 18:11:40 -0300

    feat: Criação do componente pokemon e busca de informações sobre o pokemon com axios

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
  <div id="app">
-
+  <div v-for="(poke, index) in pokemons" :key="index">
+    <Pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
+  </div>
  </div>
 </template>
 
 <script>
 
 import axios from 'axios';
+import Pokemon from './components/Pokemon';
 
 export default {
   name: 'App',
@@ -19,6 +22,9 @@ export default {
     axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res => {
       this.pokemons = res.data.results;
     })
+  },
+  components:{
+    Pokemon
   }
 }
 </script>
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>{{num}} {{capitalizedName}}</h1>
+    <small>{{url}}</small>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    created: function(){
+      axios.get(this.url).then(res =>{
+        this.pokemon.type = res.data.types[0]
+        this.pokemon.front = res.data.sprites.front_default;
+        this.pokemon.back = res.data.sprites.back_default;
+      })
+    },
+    data(){
+      return{
+        pokemon: {}
+      }
+    },
+    name: "PokemonCard",
+    props: {
+        num: Number,
+        name: String,
+        url: String
+    },
+     computed: {
+      capitalizedName() {
+        if (!this.name) return "";
+        return this.name.charAt(0).toUpperCase() + this.name.slice(1);
+      }
+    }
+}
+</script>
+
+<style>
+
+</style>
