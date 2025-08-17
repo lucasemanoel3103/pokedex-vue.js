502b45c86cbeb030348a116b2405a3ab12c0981e
Lucas emanoel <lucasemanoel625@gmail.com>
2025-08-17 19:13:10 -0300

    feat: Consumindo as imagens de cada pokemon

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,35 @@
 <template>
- <div id="app">
-  <div v-for="(poke, index) in pokemons" :key="index">
-    <Pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
+  <div id="app">
+    <div class="column is-half is-offset-one-quarter">
+      <div v-for="(poke, index) in pokemons" :key="index">
+        <Pokemon :name="poke.name" :url="poke.url" :num="index + 1" />
+      </div>
+    </div>
   </div>
- </div>
 </template>
 
 <script>
-
-import axios from 'axios';
-import Pokemon from './components/Pokemon';
+import axios from "axios";
+import Pokemon from "./components/Pokemon";
 
 export default {
-  name: 'App',
-  data(){
+  name: "App",
+  data() {
     return {
-      pokemons: []
-    }
+      pokemons: [],
+    };
   },
-  created: function(){
-    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res => {
-      this.pokemons = res.data.results;
-    })
+  created: function () {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+      .then((res) => {
+        this.pokemons = res.data.results;
+      });
   },
-  components:{
-    Pokemon
-  }
-}
+  components: {
+    Pokemon,
+  },
+};
 </script>
 
 <style>
--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,41 +1,65 @@
 <template>
-  <div>
-    <h1>{{num}} {{capitalizedName}}</h1>
-    <small>{{url}}</small>
+  <div id="pokemon">
+    <div class="card">
+      <div class="card-image">
+        <figure>
+          <img
+            :src="pokemon.front" alt="Placeholder image"/>
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4">{{ num }} - {{ capitalizedName }}</p>
+            <p class="subtitle is-6">{{ pokemon.type }}</p>
+          </div>
+        </div>
+
+        <div class="content">
+         
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-    created: function(){
-      axios.get(this.url).then(res =>{
-        this.pokemon.type = res.data.types[0]
-        this.pokemon.front = res.data.sprites.front_default;
-        this.pokemon.back = res.data.sprites.back_default;
-      })
-    },
-    data(){
-      return{
-        pokemon: {}
+  created: function () {
+    axios.get(this.url).then((res) => {
+      this.pokemon.type = res.data.types[0].type.name;
+      this.pokemon.front = res.data.sprites.front_default;
+      this.pokemon.back = res.data.sprites.back_default;
+    });
+  },
+  data() {
+    return {
+      pokemon: {
+        type: '',
+        front: '',
+        back: ''
       }
+    };
+  },
+  name: "PokemonCard",
+  props: {
+    num: Number,
+    name: String,
+    url: String,
+  },
+  computed: {
+    capitalizedName() {
+      if (!this.name) return "";
+      return this.name.charAt(0).toUpperCase() + this.name.slice(1);
     },
-    name: "PokemonCard",
-    props: {
-        num: Number,
-        name: String,
-        url: String
-    },
-     computed: {
-      capitalizedName() {
-        if (!this.name) return "";
-        return this.name.charAt(0).toUpperCase() + this.name.slice(1);
-      }
-    }
-}
+  },
+};
 </script>
 
 <style>
-
+  #pokemon{
+    margin-top: 2%;
+  }
 </style>
