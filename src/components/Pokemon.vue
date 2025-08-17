1452a029cd394fd85d33d0a33192af6aefc705a2
Lucas emanoel <lucasemanoel625@gmail.com>
2025-08-17 19:45:12 -0300

    feat: Lógica para mudança da sprite, podendo ver front e back do pokemon.

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -4,7 +4,7 @@
       <div class="card-image">
         <figure>
           <img
-            :src="pokemon.front" alt="Placeholder image"/>
+            :src="currentImg" alt="Placeholder image"/>
         </figure>
       </div>
       <div class="card-content">
@@ -14,9 +14,8 @@
             <p class="subtitle is-6">{{ pokemon.type }}</p>
           </div>
         </div>
-
         <div class="content">
-         
+         <button class="button is-medium is-fullwidth" @click="changeSprite">Mudar sprite</button>
         </div>
       </div>
     </div>
@@ -32,10 +31,13 @@ export default {
       this.pokemon.type = res.data.types[0].type.name;
       this.pokemon.front = res.data.sprites.front_default;
       this.pokemon.back = res.data.sprites.back_default;
+      this.currentImg = this.pokemon.front;
     });
   },
   data() {
     return {
+      isFront: true,
+      currentImg: '',
       pokemon: {
         type: '',
         front: '',
@@ -55,6 +57,17 @@ export default {
       return this.name.charAt(0).toUpperCase() + this.name.slice(1);
     },
   },
+  methods: {
+    changeSprite: function(){
+      if(this.isFront){
+        this.isFront = false;
+        this.currentImg =this.pokemon.back;
+      }else{
+        this.isFront = true;
+        this.currentImg = this.pokemon.front;
+      }
+    }
+  }
 };
 </script>
 
